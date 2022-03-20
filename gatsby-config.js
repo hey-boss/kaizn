38887c0bcc1adc97281cb9e6bc2f761433d5342b
yonghunj <rrallvv@gmail.com>
2022-03-20 03:40:44 -0400

    edit deploy command

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: `/kaizn`,
   siteMetadata: {
     title: `Meipaly Gatsby Js Template`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
