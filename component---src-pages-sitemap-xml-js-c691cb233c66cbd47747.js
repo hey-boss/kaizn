b20a29cfa5d147d7d1d8c583447c06d23de68299
hanioi12 <juheeko12@gmail.com>
2022-06-30 10:10:45 -0400

    Updates

---
+++ component---src-pages-sitemap-xml-js-c691cb233c66cbd47747.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[949],{6262:function(t,e,u){u.r(e),u.d(e,{default:function(){return r}});u(7294);function r(){return"Hello world"}}}]);
+//# sourceMappingURL=component---src-pages-sitemap-xml-js-c691cb233c66cbd47747.js.map
