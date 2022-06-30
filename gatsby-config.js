b92504ff03e84443b9aae5040d7b702e6838c60c
hanioi12 <juheeko12@gmail.com>
2022-06-30 09:50:20 -0400

    sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,8 @@ module.exports = {
       },
     },
     `gatsby-plugin-gatsby-cloud`,
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-netlify',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
